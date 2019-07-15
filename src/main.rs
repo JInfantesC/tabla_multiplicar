213925705f2b48c273eab9a0f4bed2ad1322c4ba
JInfantesC <jose.infantes@protonmail.com>
2019-07-15 23:27:41 +0200

    Macro para imprimir la tabla

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,47 @@ use clap::{App,Arg};
 
 fn main() {
 	let matches = App::new("Tabla de multiplicar")
-	                        .version("0.1.0")
-	                        .author("JInfantesC")
-	                        .about("Muestra la tabla de multiplicar del número definido")
-							.arg(Arg::with_name("numero")
-								.short("n")
-								.long("num")
-								.takes_value(true)
-								.help("Número de la tabla"))
-							.get_matches();
+		.version("0.1.0")
+		.author("JInfantesC")
+		.about("Muestra la tabla de multiplicar del número definido")
+		.arg(Arg::with_name("numero")
+			.short("n")
+			.long("num")
+			.takes_value(true)
+			.help("Número de la tabla"))
+		.arg(Arg::with_name("repeticiones")
+			.short("r")
+			.long("rep")
+			.takes_value(true)
+			.help("Número de resultados"))
+		.get_matches();
 	let table_of:u32 = matches.value_of("numero").unwrap_or("5").parse::<u32>().unwrap();
-	//let table_of:u32=args[1].parse::<u32>().unwrap();
-	for number in 1..11 {
-		println!(
-			"{:>width$} multiplied by {:>width$} is {:>width$}",
-			table_of, number, table_of*number, width=5);
+	let num_resultados=matches.value_of("repeticiones");
+	match num_resultados{
+		None => {
+			printTable!(table_of);
+		},
+		Some(s)=>{
+			let reps=s.parse::<u32>().unwrap();
+			printTable!(table_of, reps);
+		}
 	}
 }
-//
-// fn printTable(table_of, ){
-//
-// }
+
+#[macro_export]
+macro_rules! printTable {
+	($table_of: ident, $results: ident) => {
+		for number in 1..($results+1) {
+			println!(
+				"{:>width$} multiplied by {:>width$} is {:>width$}",
+				$table_of, number, $table_of*number, width=5);
+		}
+	};
+	($table_of: ident) => {
+		for number in 1..11 {
+			println!(
+				"{:>width$} multiplied by {:>width$} is {:>width$}",
+				$table_of, number, $table_of*number, width=5);
+		}
+	};
+}
