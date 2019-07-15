b7de10123d6a5ab3e70bbe6cff654a23af4fb69d
JInfantesC <jose.infantes@protonmail.com>
2019-07-15 22:57:25 +0200

    Enviar como argumento el número de la tabla

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::env;
+
 fn main() {
-    let table_of:u8=5;
-    for number in 1..11 {
-        println!(
-        "{} multiplied by {} is {}",
-        table_of, number, table_of*number);
-    }
+	let args: Vec<String> = env::args().collect();
+	if args.len()>1{
+		let table_of:u32=args[1].parse::<u32>().unwrap();
+		for number in 1..11 {
+			println!(
+				"{} multiplied by {} is {}",
+				table_of, number, table_of*number);
+		}
+	}else{
+		println!("Please, give me a number")
+	}
 }
